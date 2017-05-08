a33e24676ffed8c866988d6f7dbe3615eb21757d
adityaridha <adityaridharrahman@gmail.com>
2017-05-08 16:26:43 +0700

    test_cases creation

--- test_cases_account.py
+++ test_cases_account.py
@@ -1,5 +1,4 @@
 import pytest
-import requests
 from helper import *
 from hub3c_api import *
 
@@ -9,19 +8,20 @@ __email__       = "aditya.ridharrahman@geekseat.com.au"
 __status__      = "development"
 __last_update__ = "10th Feb 2017"
 
-print("######### Execute test cases #########")
+print("######### Execute Account test cases #########")
 
 class TestAccount():
 
     account_api = AccountAPI()
     util = Util()
 
-    def test_register(self):
-        pass
 
     def test_login(self):
         result = self.account_api.login()
         self.util.print_response(result)
+
+    def test_register(self):
+        pass
 
     def test_account_list(self):
         pass
@@ -31,13 +31,11 @@ class TestAccount():
         result = self.account_api.logout()
         self.util.print_response(result)
 
-class TestUtility():
+    def switch_account(self):
+        pass
 
-    util = Util()
-
-    def test_time_zone(self):
-        result = requests.get(url='http://119.9.53.234:6969/api/v1/Utility/TimeZoneList')
-        self.util.print_response(result)
+    def forgot_password(self):
+        pass
 
 
 
